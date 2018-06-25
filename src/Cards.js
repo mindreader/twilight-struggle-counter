41a635fead4f21934cdb5d192912fc54219285ad
David McHealy <david.mchealy@gmail.com>
2018-06-25 08:52:07 -0400

    Renaming keys for cards to be more memorable.

--- src/Cards.js
+++ src/Cards.js
@@ -3,7 +3,14 @@ const { Map } = require("immutable");
 class Cards {
   static cards = () =>
     Map({
-      as: Map({
+      japan: Map({
+        name: "us / japan pact",
+        early: true,
+        event: true,
+        side: "us",
+        ops: 4
+      }),
+      asia: Map({
         name: "asia scoring",
         early: true,
         event: false,
@@ -32,21 +39,21 @@ class Cards {
         side: "neutral",
         scoringcard: true
       }),
-      ra: Map({
+      romabd: Map({
         name: "roman abdication",
         early: true,
         event: true,
         side: "ussr",
         ops: 1
       }),
-      ir: Map({
+      ireds: Map({
         name: "independent reds",
         early: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      og: Map({
+      olymp: Map({
         name: "olympic games",
         early: true,
         event: true,
@@ -61,21 +68,21 @@ class Cards {
         side: "neutral",
         ops: 4
       }),
-      br: Map({
+      bw: Map({
         name: "brush war",
         mid: true,
         event: false,
         side: "neutral",
         ops: 2
       }),
-      wb: Map({
+      willy: Map({
         name: "willy brandt",
         mid: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      dc: Map({
+      "d&c": Map({
         name: "duck and cover",
         early: true,
         event: false,
@@ -89,49 +96,49 @@ class Cards {
         side: "us",
         ops: 3
       }),
-      sg: Map({
+      socgov: Map({
         name: "socialist governments",
         early: true,
         event: false,
         side: "ussr",
         ops: 3
       }),
-      ys: Map({
+      yuri: Map({
         name: "yuri & samantha",
         late: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      fd: Map({
+      fidel: Map({
         name: "fidel",
         early: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      vr: Map({
+      viet: Map({
         name: "vietnam revolts",
         early: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      bl: Map({
+      block: Map({
         name: "blockade",
         early: true,
         event: true,
         side: "ussr",
         ops: 1
       }),
-      kw: Map({
+      korw: Map({
         name: "korean war",
         early: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      aiw: Map({
+      arisw: Map({
         name: "arab-israeli war",
         early: true,
         event: false,
@@ -145,21 +152,21 @@ class Cards {
         side: "ussr",
         ops: 3
       }),
-      nsr: Map({
+      nass: Map({
         name: "nasser",
         early: true,
         event: true,
         side: "ussr",
         ops: 1
       }),
-      wsp: Map({
+      warsaw: Map({
         name: "warsaw pact formed",
         early: true,
         event: true,
         side: "ussr",
         ops: 3
       }),
-      deg: Map({
+      degau: Map({
         name: "degaulle leads france",
         early: true,
         event: true,
@@ -180,7 +187,7 @@ class Cards {
         side: "us",
         ops: 1
       }),
-      nt: Map({ name: "nato", early: true, event: true, side: "us", ops: 4 }),
+      nato: Map({ name: "nato", early: true, event: true, side: "us", ops: 4 }),
       mp: Map({
         name: "marshall plan",
         early: true,
@@ -188,14 +195,14 @@ class Cards {
         side: "us",
         ops: 4
       }),
-      ipw: Map({
+      indopw: Map({
         name: "indo-pakistani war",
         early: true,
         event: false,
         side: "neutral",
         ops: 2
       }),
-      cnt: Map({
+      cont: Map({
         name: "containment",
         early: true,
         event: true,
@@ -223,7 +230,7 @@ class Cards {
         side: "us",
         ops: 3
       }),
-      dcz: Map({
+      decol: Map({
         name: "decolonization",
         early: true,
         event: false,
@@ -258,35 +265,35 @@ class Cards {
         side: "us",
         ops: 2
       }),
-      cas: Map({
+      cent: Map({
         name: "central american scoring",
         mid: true,
         event: false,
         side: "neutral",
         scoringcard: true
       }),
-      ar: Map({
+      armsr: Map({
         name: "arms race",
         mid: true,
         event: false,
         side: "neutral",
         ops: 3
       }),
-      cmc: Map({
+      cubmc: Map({
         name: "cuban missile crisis",
         mid: true,
         event: true,
         side: "neutral",
         ops: 3
       }),
-      subs: Map({
+      nsubs: Map({
         name: "nuclear subs",
         mid: true,
         event: true,
         side: "us",
         ops: 2
       }),
-      qm: Map({
+      quag: Map({
         name: "quagmire",
         mid: true,
         event: true,
@@ -300,42 +307,42 @@ class Cards {
         side: "neutral",
         ops: 3
       }),
-      bt: Map({
+      bear: Map({
         name: "bear trap",
         mid: true,
         event: true,
         side: "us",
         ops: 3
       }),
-      sum: Map({
+      summ: Map({
         name: "summit",
         mid: true,
         event: false,
         side: "neutral",
         ops: 1
       }),
-      hil: Map({
+      howil: Map({
         name: "how i learned to stop worrying",
         mid: true,
         event: true,
         side: "neutral",
         ops: 2
       }),
-      jun: Map({
+      junta: Map({
         name: "junta",
         mid: true,
         event: false,
         side: "neutral",
         ops: 2
       }),
-      kd: Map({
+      kitch: Map({
         name: "kitchen debates",
         mid: true,
         event: true,
         side: "us",
         ops: 1
       }),
-      mis: Map({
+      misenv: Map({
         name: "missile envy",
         mid: true,
         event: false,
@@ -356,14 +363,14 @@ class Cards {
         side: "ussr",
         ops: 3
       }),
-      pec: Map({
-        name: "portuuese empire crumbles",
+      port: Map({
+        name: "portuguese empire crumbles",
         mid: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      sau: Map({
+      safrun: Map({
         name: "south african unrest",
         mid: true,
         event: false,
@@ -377,7 +384,7 @@ class Cards {
         side: "ussr",
         ops: 1
       }),
-      mus: Map({
+      musrev: Map({
         name: "muslim revolution",
         mid: true,
         event: false,
@@ -391,7 +398,7 @@ class Cards {
         side: "ussr",
         ops: 3
       }),
-      fp: Map({
+      flowr: Map({
         name: "flower power",
         mid: true,
         event: true,
@@ -412,7 +419,7 @@ class Cards {
         side: "ussr",
         ops: 3
       }),
-      lg: Map({
+      lone: Map({
         name: "lone gunman",
         mid: true,
         event: true,
@@ -426,35 +433,35 @@ class Cards {
         side: "us",
         ops: 2
       }),
-      pcr: Map({
+      panam: Map({
         name: "panama canal returned",
         mid: true,
         event: true,
         side: "us",
         ops: 1
       }),
-      cda: Map({
+      camp: Map({
         name: "camp david accords",
         mid: true,
         event: true,
         side: "us",
         ops: 2
       }),
-      pg: Map({
+      pupp: Map({
         name: "puppet governments",
         mid: true,
         event: true,
         side: "us",
         ops: 2
       }),
-      gs: Map({
+      gsales: Map({
         name: "grain sales to soviets",
         mid: true,
         event: false,
         side: "us",
         ops: 2
       }),
-      jp: Map({
+      jp2: Map({
         name: "john paul II elected pope",
         mid: true,
         event: true,
@@ -482,14 +489,14 @@ class Cards {
         side: "us",
         ops: 1
       }),
-      sad: Map({
+      saddat: Map({
         name: "saddat expels soviets",
         mid: true,
         event: true,
         side: "us",
         ops: 1
       }),
-      shu: Map({
+      shutt: Map({
         name: "shuttle diplomacy",
         mid: true,
         event: true,
@@ -510,7 +517,7 @@ class Cards {
         side: "ussr",
         ops: 2
       }),
-      urs: Map({
+      ussu: Map({
         name: "ussuri river skirmish",
         mid: true,
         event: true,
@@ -524,14 +531,14 @@ class Cards {
         side: "us",
         ops: 3
       }),
-      pro: Map({
+      allpro: Map({
         name: "alliance for progress",
         mid: true,
         event: true,
         side: "us",
         ops: 3
       }),
-      afs: Map({
+      afr: Map({
         name: "african scoring",
         mid: true,
         event: false,
@@ -545,28 +552,28 @@ class Cards {
         side: "neutral",
         ops: 2
       }),
-      sas: Map({
+      soam: Map({
         name: "south america scoring",
         mid: true,
         event: false,
         side: "neutral",
         scoringcard: true
       }),
-      host: Map({
+      hostage: Map({
         name: "iranian hostage crisis",
         late: true,
         event: true,
         side: "ussr",
         ops: 3
       }),
-      il: Map({
+      ironl: Map({
         name: "the iron lady",
         late: true,
         event: true,
         side: "us",
         ops: 3
       }),
-      rbl: Map({
+      reagan: Map({
         name: "reagan bombs libya",
         late: true,
         event: true,
@@ -580,21 +587,21 @@ class Cards {
         side: "us",
         ops: 2
       }),
-      nso: Map({
+      norsea: Map({
         name: "north sea oil",
         late: true,
         event: true,
         side: "us",
         ops: 3
       }),
-      ref: Map({
+      refo: Map({
         name: "the reformer",
         late: true,
         event: true,
         side: "ussr",
         ops: 3
       }),
-      mbb: Map({
+      marine: Map({
         name: "marine barracks bombing",
         late: true,
         event: true,
@@ -615,28 +622,28 @@ class Cards {
         side: "ussr",
         ops: 4
       }),
-      ort: Map({
+      ortega: Map({
         name: "ortega elected in nicaragua",
         late: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      ter: Map({
+      terr: Map({
         name: "terrorism",
         late: true,
         event: false,
         side: "neutral",
         ops: 2
       }),
-      ics: Map({
+      contra: Map({
         name: "iron-contra scandal",
         late: true,
         event: true,
         side: "ussr",
         ops: 2
       }),
-      cnb: Map({
+      chern: Map({
         name: "chernobyl",
         late: true,
         event: true,
@@ -650,42 +657,42 @@ class Cards {
         side: "ussr",
         ops: 2
       }),
-      tdw: Map({
+      tdtw: Map({
         name: "tear down this wall",
         late: true,
         event: true,
         side: "us",
         ops: 3
       }),
-      evl: Map({
+      evil: Map({
         name: "an evil empire",
         late: true,
         event: true,
         side: "us",
         ops: 3
       }),
-      ald: Map({
+      aldr: Map({
         name: "aldrich ames remix",
         late: true,
         event: true,
         side: "ussr",
         ops: 3
       }),
-      per: Map({
+      persh: Map({
         name: "pershing II deployed",
         late: true,
         event: true,
         side: "ussr",
         ops: 3
       }),
-      wg: Map({
+      warg: Map({
         name: "wargames",
         late: true,
         event: true,
         side: "neutral",
         ops: 4
       }),
-      sol: Map({
+      solid: Map({
         name: "solidarity",
         late: true,
         event: true,
@@ -699,23 +706,23 @@ class Cards {
         side: "neutral",
         ops: 2
       }),
-      cam: Map({
+      camb: Map({
         name: "the cambridge five",
         late: true,
         event: false,
         side: "ussr",
         ops: 2
       }),
-      sr: Map({
+      specr: Map({
         name: "special relationship",
         late: true,
         event: false,
         side: "us",
         ops: 2
       }),
-      nrd: Map({ name: "norad", late: true, event: true, side: "us", ops: 3 }),
+      nord: Map({ name: "norad", late: true, event: true, side: "us", ops: 3 }),
       che: Map({ name: "che", late: true, event: true, side: "ussr", ops: 3 }),
-      omt: Map({
+      tehran: Map({
         name: "our man in tehran",
         late: true,
         event: true,
@@ -732,109 +739,126 @@ class Cards {
     });
 
   static cardsWithImportance = () =>
-    Cards.cards()
-      .map((c, k) => {
-        switch (k) {
-          case "ra":
-          case "ir":
-          case "og":
-          case "ys":
-          case "il":
-          case "nso":
-          case "shu":
-          case "eeu":
-          case "awac":
-          case "evl":
-          case "rbl":
-          case "kd":
-          case "one":
-          case "cns":
-          case "iiw":
-          case "lads":
-          case "ladc":
-          case "sum":
-          case "come":
-            return c.set("importance", 1);
+    Cards.cards().map((c, k) => {
+      switch (k) {
+        case "romabd":
+        case "ireds":
+        case "olymp":
+        case "yuri":
+        case "ironl":
+        case "norsea":
+        case "shutt":
+        case "eeu":
+        case "awac":
+        case "evil":
+        case "reagan":
+        case "kitch":
+        case "one":
+        case "cns":
+        case "iiw":
+        case "lads":
+        case "ladc":
+        case "summ":
+        case "come":
+        case "persh":
+        case "u2":
+        case "contra":
+        case "viet":
+          return c.set("importance", 1);
 
-          case "wb":
-          case "cnt":
-          case "pro":
-          case "cda":
-          case "jp":
-          case "form":
-          case "sol":
-          case "omt":
-          case "sr":
-          case "ar":
-          case "cmc":
-          case "hil":
-          case "ipw":
-          case "un":
-          case "brez":
-          case "host":
-          case "wsp":
-            return c.set("importance", 2);
+        case "willy":
+        case "cont":
+        case "camp":
+        case "jp2":
+        case "form":
+        case "solid":
+        case "tehran":
+        case "specr":
+        case "armsr":
+        case "cubmc":
+        case "howil":
+        case "indopw":
+        case "un":
+        case "brez":
+        case "hostage":
+        case "warsaw":
+        case "marine":
+        case "port":
+        case "arisw":
+        case "camb":
+        case "ortega":
+        case "fidel":
+        case "allpro":
+          return c.set("importance", 2);
 
-          case "dc":
-          case "sea":
-          case "kal":
-          case "tdw":
-          case "ask":
-          case "cnb":
-          case "fyp":
-          case "sw":
-          case "subs":
-          case "pg":
-          case "nix":
-          case "oas":
-          case "trum":
-          case "sad":
-          case "pcr":
-          case "salt":
-          case "ter":
-          case "mis":
-          case "fp":
-          case "mus":
-          case "glas":
-          case "che":
-          case "opec":
-          case "deg":
-          case "cult":
-          case "sg":
-          case "suez":
-          case "qm":
-            return c.set("importance", 3);
+        case "d&c":
+        case "sea":
+        case "kal":
+        case "tdtw":
+        case "ask":
+        case "chern":
+        case "fyp":
+        case "sw":
+        case "nsubs":
+        case "pupp":
+        case "nix":
+        case "oas":
+        case "trum":
+        case "saddat":
+        case "panam":
+        case "salt":
+        case "terr":
+        case "misenv":
+        case "flowr":
+        case "musrev":
+        case "glas":
+        case "che":
+        case "opec":
+        case "degau":
+        case "cult":
+        case "socgov":
+        case "suez":
+        case "quag":
+        case "safrun":
+        case "korw":
+        case "nass":
+          return c.set("importance", 3);
 
-          case "as":
-          case "rsp":
-          case "br":
-          case "mp":
-          case "nt":
-          case "nrd":
-          case "bt":
-          case "urs":
-          case "def":
-          case "voa":
-          case "gs":
-          case "crg":
-          case "cia":
-          case "sas":
-          case "afs":
-          case "eu":
-          case "cas":
-          case "mes":
-          case "ntb":
-          case "wg":
-          case "jun":
-          case "wwby":
-          case "ref":
-          case "ald":
-            return c.set("importance", 4);
+        case "asia":
+        case "rsp":
+        case "bw":
+        case "mp":
+        case "nato":
+        case "nord":
+        case "bear":
+        case "ussu":
+        case "def":
+        case "voa":
+        case "gsales":
+        case "crg":
+        case "cia":
+        case "soam":
+        case "afr":
+        case "eu":
+        case "cent":
+        case "mes":
+        case "ntb":
+        case "warg":
+        case "junta":
+        case "wwby":
+        case "refo":
+        case "aldr":
+        case "lib":
+        case "decol":
+        case "block":
+        case "lone":
+        case "japan":
+        case "all":
+          return c.set("importance", 4);
 
-          default:
-            return c.set("importance", 0);
-        }
-      })
-      .filter(c => !c.get("importance"));
+        default:
+          return c.set("importance", 0);
+      }
+    });
 }
 export default Cards;
