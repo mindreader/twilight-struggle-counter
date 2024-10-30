fe99d029e54aea4f0b0a3481d226ca9d867c95fd
David McHealy <david.mchealy@gmail.com>
2024-10-30 10:01:06 -0400

    fix: This can never be null.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-// @ts-nocheck
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
