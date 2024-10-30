3cb77a32d15d4b27c0a8c33f05d90230db42064d
David McHealy <david.mchealy@gmail.com>
2024-10-30 10:06:01 -0400

    fix: refactor saving and loading.

--- src/Save.ts
+++ src/Save.ts
@@ -1,40 +1,33 @@
-// @ts-nocheck
-
 class Download {
-
   static download = (data) => {
     var blob = new Blob([JSON.stringify(data)], {type: 'text/plain'});
     const filename = prompt("filename")
     if (filename) {
-      if(window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveBlob(blob, filename);
-      }
-      else{
-        var elem = window.document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.download = filename
-        document.body.appendChild(elem);
-        elem.click();        
-        document.body.removeChild(elem);
-      }
+      var elem = window.document.createElement('a');
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = filename;
+      document.body.appendChild(elem);
+      elem.click();
+      document.body.removeChild(elem);
     }
   }
 }
 
 class Load {
-  static load = (files, onsuccess) => {
+  static load = (files: File[], onsuccess: (data: any) => void) => {
       for (var i = 0; i < files.length; ++i) {
 
-        const f = (file) => {               // Wrap current file in a closure.
+        const f = (file: File) => {               // Wrap current file in a closure.
           var loader = new FileReader();
-          loader.onload = function (loadEvent) {
-            if (loadEvent.target.readyState !== 2)
+          loader.onload = function (loadEvent: ProgressEvent<FileReader>) {
+            const target = loadEvent.target as FileReader;
+            if (target.readyState !== 2)
               return;
-            if (loadEvent.target.error) {
-              console.log("Error while reading file " + file.name + ": " + loadEvent.target.error);
+            if (target.error) {
+              console.log("Error while reading file " + file.name + ": " + target.error);
               return;
             }
-            onsuccess(JSON.parse(loadEvent.target.result))
+            onsuccess(JSON.parse(target.result as string))
           };
           loader.onabort = function () {
             console.log("aborted")
